52b0e138e0268af3b1c694fd0688bc148ed6066b
Obai Alnajjar <obai@outlook.com>
2019-02-18 03:56:31 +0300

    Revert "added body-parser"
    This reverts commit a263555cfcc2942cdbfa2f2c05b331c61d1abdbf.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 
@@ -17,8 +16,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
