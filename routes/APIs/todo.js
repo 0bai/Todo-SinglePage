fd3bf3957e0e49bdd6d5f913b786eda72bd6292e
Obai Alnajjar <obai@outlook.com>
2019-02-18 04:14:15 +0300

    Added comments and status codes

--- routes/APIs/todo.js
+++ routes/APIs/todo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../models');
 
-
+//Retrieve all tasks
 router.get('/', function (req, res) {
 	db.Task.find()
 		.then(function (tasks) {
@@ -13,12 +13,12 @@ router.get('/', function (req, res) {
 		});
 });
 
-
+//Create a task
 router.post('/', function (req, res) {
 	console.log(req.body);
 	db.Task.create(req.body)
 		.then(function (newTask) {
-			res.json(newTask);
+			res.status(201).json(newTask);
 		}).catch(function (err) {
 		res.send(err);
 	});
