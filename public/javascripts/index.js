3dacc09ddc0d8c1240e83dbc4568e13ca967f392
Obai Alnajjar <obai@outlook.com>
2019-02-23 07:38:53 +0300

    Added Delete & Update task functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,14 +10,26 @@ $(document).ready(function () {
 			createTask();
 		}
 	});
+	
+	$('.list').on('click', 'span', function (e) {
+		e.stopPropagation();
+		removeTask($(this).parent());
+	});
+	
+	$('.list').on('click', 'li', function () {
+		updateTask($(this));
+	});
 });
+
 
 function addTasks(tasks) {
 	tasks.forEach(addTask);
 }
 
 function addTask(task) {
-	var newTask = $('<li class="task">' + task.name + '</li>');
+	var newTask = $('<li class="task">' + task.name + '<span>X</span></li>');
+	newTask.data('id', task._id);
+	newTask.data('completed', task.completed);
 	if (task.completed) newTask.addClass('done');
 	$('.list').append(newTask);
 }
@@ -27,4 +39,29 @@ function createTask() {
 	$.post('api/todo', {'name': name})
 		.then(addTask);
 	$('#todoInput').val('');
+}
+
+function removeTask(task) {
+	var id = task.data('id');
+	$.ajax({
+			method: 'DELETE',
+			url: 'api/todo/' + id
+		})
+		.then(function (data) {
+			task.remove();
+		});
+}
+
+function updateTask(task) {
+	$.ajax('/api/todo/'+task.data('id'),{
+		method: 'PUT',
+		data:{'completed': !task.data('completed')}
+	})
+		.then(function (data) {
+			task.toggleClass('done');
+			task.data('completed', !task.data('completed'));
+		})
+		.catch(function (err) {
+			console.log(err);
+		})
 }
