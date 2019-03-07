d8791c6a10034ea9df57ad7ec6157ee743fc90af
Obai Alnajjar <obai@outlook.com>
2019-03-07 22:04:59 +0300

    Added connection to remote DB

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/todo-api");
-
+// mongoose.connect("mongodb://localhost/todo-api");
+mongoose.connect("mongodb://35.185.97.76:80/todo-api");
 mongoose.Promise = Promise;
 
 module.exports.Task = require('./task');
